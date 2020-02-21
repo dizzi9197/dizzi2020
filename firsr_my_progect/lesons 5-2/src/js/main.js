d7ef76ddecf0a4a1e629d07f4b5748c1ab759501
dizzi <64102bv@gmail.com>
2020-02-21 12:27:52 +0200

    Урок 5 задание 2

---
+++ firsr_my_progect/lesons 5-2/src/js/main.js
@@ -0,0 +1,21 @@
+let start = document.getElementById('start');
+let budgetValue = document.getElementsByClassName('budget-value');
+let daybudgetValue = document.getElementsByClassName('daybudget-value');
+let levelValue = document.getElementsByClassName('level-value');
+let expensesValue = document.getElementsByClassName('expenses-value');
+let optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value');
+let incomeValue = document.getElementsByClassName('income-value');
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value');
+let yearsavingsValue = document.getElementsByClassName('yearsavings-value');
+let expensesItem = document.getElementsByClassName('expenses-item');
+let expensesItemBtn = document.getElementsByTagName('button')[0];
+let countBudgetBtn = document.getElementsByTagName('button')[2];
+let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+let chooseIncomeLabel = document.querySelector('.choose-income-label');
+let choosePercent = document.querySelector('.choose-percent');
+let chooseSum = document.querySelector('.choose-sum');
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
+console.log(dayValue);
